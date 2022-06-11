53986ef9814db6a6427208904bd6328002b8c49d
Emilio <emiliomtg@gmail.com>
2022-06-11 19:53:15 +0100

    cambios en el home

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import './index.css'
+
+import Home from './components/Home'
+import Clientes from './components/Clientes'
+import Productos from './components/Productos'
+import Facturas from './components/Facturas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
 	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/facturas" element={<Facturas />} />
-		<Route path="/clientes" element={<Clientes />} />
-		<Route path="/productos" element={<Productos />} />
+		<Route path="/" element={<Home />} >
+			<Route path="/facturas" element={<Facturas />} />
+			<Route path="/clientes" element={<Clientes />} />
+			<Route path="/productos" element={<Productos />} />
+		</Route>
 	</Routes>
  </BrowserRouter>
 )
